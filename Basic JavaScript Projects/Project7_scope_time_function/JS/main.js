a52452b51861b8a5374a7dcc2ff24b65c1ea3e11
LivedDiagram292 <120764062+justdan1234@users.noreply.github.com>
2023-08-18 12:53:11 -0700

    Project7_scope_time_function

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,49 @@
+//Assignining local and global variables
+var x = 10; //global variable
+
+function Add_numbers_1() {
+    var x = 99; //local variable
+    document.write(10 + x + "<br>"); //using local variable
+}
+
+function Add_numbers_2() {
+    document.write(x + 11); //using global variable
+    console.log(x);//using console.log to debug which variable is being used
+}
+
+Add_numbers_1();
+Add_numbers_2();
+
+//A function that icludes an if statement
+function get_Date() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("Greeting").innerhtml = "It is currently before 6pm";
+    }
+}
+
+//A function with if and else statements
+function get_Reply() {
+    var user_input = document.getElementById("userInput").value;
+    if (user_input % 2 ===0) {
+        reply = "you entered an even number!";
+        document.getElementById("feedback").innerHTML = reply;
+    }
+    else {
+        reply = "You entered an odd number!";
+        document.getElementById("feedback").innerHTML = reply;
+    }
+}
+
+//Utilizing the time function() from previous slides
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    } else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon!";
+    } else {
+        Reply = "It is evening time!";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
